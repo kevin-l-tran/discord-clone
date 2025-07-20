e3b93bb7a454f660aca1b81e33424e9d6255931e
kevin-l-tran <kltran@csu.fullerton.edu>
2025-07-19 23:05:05 -0700

    refactor: swapped out flask-mongoengine for mongoengine

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,22 +1,20 @@
 import os
 
 from flask import Flask
-from flask_mongoengine import MongoEngine
+from mongoengine import connect
 from dotenv import load_dotenv
 
 load_dotenv()
 
-db = MongoEngine()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'temporary key')
-    
-    app.config['MONGODB_SETTINGS'] = {
-        'db':       os.getenv('MONGO_DB', 'my_database'),
-        'host':     os.getenv('MONGO_HOST', 'localhost'),
-        'port':     int(os.getenv('MONGO_PORT', 27017)),
-    }
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "temporary key")
 
-    db.init_app(app)
+    connect(
+        db=os.getenv("MONGO_DB", "my_database"),
+        host=os.getenv("MONGO_HOST", "localhost"),
+        port=int(os.getenv("MONGO_PORT", 27017)),
+    )
+
     return app
