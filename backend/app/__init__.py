282878a6ddc3837d7554bf77550551bbd4d76430
kevin-l-tran <kltran@csu.fullerton.edu>
2025-07-20 03:36:14 -0700

    feat: added login route with jwt auth

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,18 +1,25 @@
 import os
+import datetime
 
 from flask import Flask
 from mongoengine import connect
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 
 flask_bcrypt = Bcrypt()
+jwt = JWTManager()
 
 load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
+
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "temporary key")
+
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "temporary key")
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(hours=1)
 
     connect(
         db=os.getenv("MONGO_DB", "my_database"),
@@ -21,5 +28,6 @@ def create_app():
     )
 
     flask_bcrypt.init_app(app)
+    jwt.init_app(app)
 
     return app
---
+++ backend/app/auth.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, request, jsonify
+from mongoengine.errors import DoesNotExist, MultipleObjectsReturned
+from flask_jwt_extended import create_access_token
+
+from models import User
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['POST'])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    if not email:
+        return jsonify({"msg": "Missing email"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password"}), 400
+    
+    try:
+        user = User.objects.get(email=email)
+    except DoesNotExist:
+        return jsonify({"msg": "Bad email"}), 401
+    except MultipleObjectsReturned:
+        return jsonify({"msg": "DB integrity broken (identical emails)"}), 500
+    else:
+        if not user.check_password(password):
+            return jsonify({"msg": "Bad password"}), 401
+        
+        access_token = create_access_token(identity=email)
+        return jsonify(access_token=access_token), 200
