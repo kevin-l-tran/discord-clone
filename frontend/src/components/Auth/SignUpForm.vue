a26867ef08fe1cd8ad995bdf481916d75737cbe3
kevin-l-tran <kltran@csu.fullerton.edu>
2025-07-25 04:36:35 -0700

    feat: added signin backend call with validity checks

--- frontend/src/components/Auth/SignUpForm.vue
+++ frontend/src/components/Auth/SignUpForm.vue
@@ -142,8 +142,8 @@ async function signup() {
 }
 
 function _handleNetworkError(err: unknown) {
-    console.error(err)
-    alert('An unexpected network error occurred. Please try again later.')
+    console.error(err);
+    alert('An unexpected network error occurred. Please try again later.');
 }
 
 </script>
@@ -158,12 +158,12 @@ function _handleNetworkError(err: unknown) {
         <input type="text" placeholder="Username" id="username" v-model="username" ref="usernameInput"
             class="w-full mb-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-400" />
         <div class="text-xs self-end mb-3" :class="!usernameWarn ? 'text-gray-500' : 'text-red-500'">{{ !usernameWarn ?
-            "Username must be over 3 characters long." : usernameWarn }}</div>
+            "Username must be over 3 characters long" : usernameWarn }}</div>
         <input type="password" placeholder="Password" id="password" v-model="password" ref="passwordInput"
             class="w-full mb-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-400" />
         <div class="text-xs self-end mb-5" :class="!passwordWarn ? 'text-gray-500' : 'text-red-500'">{{
             !passwordWarn ?
-                "Password must be over 7 characters long." : passwordWarn }}</div>
+                "Password must be over 7 characters long" : passwordWarn }}</div>
         <button
             class="cursor-pointer w-full py-3 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-800 transition-colors duration-500">
             Sign Up
